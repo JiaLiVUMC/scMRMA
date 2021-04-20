c3c04f5c53d844b0026b7689a79d437ba85f8519
jiali-vu <58566027+jiali-vu@users.noreply.github.com>
2021-04-20 15:21:08 -0500

    Add files via upload

---
+++ src/preprocessing.cpp
@@ -0,0 +1,80 @@
+#include <RcppEigen.h>
+#include <progress.hpp>
+#include <cmath>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+
+using namespace Rcpp;
+// [[Rcpp::depends(RcppEigen)]]
+// [[Rcpp::depends(RcppProgress)]]
+
+//' normalize single cell count data
+//' @param data count data
+//' @export
+//' 
+// [[Rcpp::export]]
+Eigen::SparseMatrix<double> LogNorm(Eigen::SparseMatrix<double> data, bool display_progress = false){
+  Progress p(data.outerSize(), display_progress);
+  Eigen::VectorXd colSums = data.transpose() * Eigen::VectorXd::Ones(data.rows());
+  for (int k=0; k < data.outerSize(); ++k){
+    p.increment();
+    for (Eigen::SparseMatrix<double>::InnerIterator it(data, k); it; ++it){
+      it.valueRef() = log1p(double(it.value()) / colSums[k] * 10000);
+    }
+  }
+  return data;
+}
+
+//' variance calculation
+//' @export
+//' 
+// [[Rcpp::export]]
+NumericVector SparseRowVar2(Eigen::SparseMatrix<double> mat,
+                            NumericVector mu,
+                            bool display_progress = false){
+  mat = mat.transpose();
+  Progress p(mat.outerSize(), display_progress);
+  NumericVector allVars = no_init(mat.cols());
+  for (int k=0; k<mat.outerSize(); ++k){
+    p.increment();
+    double colSum = 0;
+    int nZero = mat.rows();
+    for (Eigen::SparseMatrix<double>::InnerIterator it(mat,k); it; ++it) {
+      nZero -= 1;
+      colSum += pow(it.value() - mu[k], 2);
+    }
+    colSum += pow(mu[k], 2) * nZero;
+    allVars[k] = colSum / (mat.rows() - 1);
+  }
+  return(allVars);
+}
+
+//' variance calculation
+//' @export
+//' 
+// [[Rcpp::export]]
+NumericVector SparseRowVarStd(Eigen::SparseMatrix<double> mat,
+                              NumericVector mu,
+                              NumericVector sd,
+                              double vmax,
+                              bool display_progress = false){
+  mat = mat.transpose();
+  NumericVector allVars(mat.cols());
+  Progress p(mat.outerSize(), display_progress);
+  for (int k=0; k<mat.outerSize(); ++k){
+    p.increment();
+    if (sd[k] == 0) continue;
+    double colSum = 0;
+    int nZero = mat.rows();
+    for (Eigen::SparseMatrix<double>::InnerIterator it(mat,k); it; ++it)
+    {
+      nZero -= 1;
+      colSum += pow(std::min(vmax, (it.value() - mu[k]) / sd[k]), 2);
+    }
+    colSum += pow((0 - mu[k]) / sd[k], 2) * nZero;
+    allVars[k] = colSum / (mat.rows() - 1);
+  }
+  return(allVars);
+}
+
